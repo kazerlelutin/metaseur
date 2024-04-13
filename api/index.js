d009c182fe8815ebbda95cab6a0261a9f77c88be
Kazerlelutin <b@bouteiller.contact>
2024-04-14 01:58:03 +0200

    first version

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import { JSDOM } from 'jsdom'
+export default async function meta(req, res) {
+  const url = req.query?.url
+  if (!url) return res.status(400).json({ error: 'URL is required' })
+
+  const response = await fetch(url)
+  const data = await response.text()
+  const dom = new JSDOM(data)
+  const doc = dom.window.document
+
+  const meta = {
+    title: doc.querySelector('title').textContent,
+    description: doc.querySelector('meta[name="description"]')?.content || '',
+    image: doc.querySelector('meta[property="og:image"]')?.content || '',
+    url: doc.querySelector('meta[property="og:url"]')?.content || '',
+    favicon:
+      doc.querySelector('link[rel="shortcut icon"]')?.href ||
+      doc.querySelector('link[rel="icon"]')?.href ||
+      '',
+  }
+
+  return res.status(200).json(meta)
+}
--- api/meta/index.js
+++
@@ -1,5 +0,0 @@
-export default async function search(req, res) {
-  const url = req.params.url
-
-  return res.status(200).json({ url })
-}
